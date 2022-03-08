08fb810bae077151ee489804fa177caf6fa7d067
acuedd <acuedd@gmail.com>
2022-03-07 19:35:57 -0600

    FEAT * change sql dependencies, by mongodb * Change all funcionality to adapt it to mongodb

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,19 +13,24 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user.createProduct({
-    title:title,
-    price: price,
-    imageUrl: imageUrl, 
-    description: description
-  })  
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
+  product
+    .save()
     .then(result => {
-      //console.log(result);
-      console.log("Created Product")
+      // console.log(result);
+      console.log('Created Product');
       res.redirect('/admin/products');
     })
-    .catch(err => console.log(err));
-  
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -34,11 +39,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  req.user
-    .getProducts({ where: { id: prodId }} )
-  //Product.findByPk(prodId)
-    .then(products => {
-      const product = products[0];
+  Product.findById(prodId)
+    // Product.findById(prodId)
+    .then(product => {
       if (!product) {
         return res.redirect('/');
       }
@@ -58,45 +61,41 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  Product.findByPk(prodId)
-    .then( product =>{
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
+
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    prodId
+  );
+  product
+    .save()
     .then(result => {
-      console.log("updated product")
+      console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
-    .catch(err => console.log(err));  
+    .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  //Product.findAll()
-  req.user.getProducts()
-  .then(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
-    });
-  })
-  .catch(err =>{
-    console.log(err);
-  });
+  Product.fetchAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId)
-    .then(product =>{
-      return product.destroy();
-    })
-    .then(result =>{
+  Product.deleteById(prodId)
+    .then(() => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
     })
-    .catch( err => console.log(err));  
+    .catch(err => console.log(err));
 };
--- util/database.js
+++ util/database.js
@@ -1,17 +1,26 @@
-/*const mysql = require("mysql2");
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const pool = mysql.createPool({
-    host: '127.0.0.1', 
-    user: 'root',
-    database: 'node-complete', 
-    password: 'hmlDoUrden'
-});
+let _db;
 
-module.exports = pool.promise();*/
+const mongoConnect = (callback) =>{
+    MongoClient.connect('mongodb://root:hmlDoUrden@localhost:27017/shop?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false')
+    .then(client => {
+        console.log('connected!!');
+        _db = client.db('shop');
+        callback(client);
+    })
+    .catch( err => {
+        console.log(err);
+    });
+};
 
-const Sequelize = require('sequelize');
+const getDb = () => {
+    if(_db){
+        return _db;
+    }
+    throw 'No database found!';
+}
 
-const sequelize = new Sequelize('node-complete','root', 'hmlDoUrden', 
-{ dialect: 'mysql', host: '127.0.0.1'});
-
-module.exports = sequelize;
+exports.mongoConnect = mongoConnect;
+module.exports.getDb = getDb;
